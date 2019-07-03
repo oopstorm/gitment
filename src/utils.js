c0f6c36683afc2306fbcc17cd59dda459642c746
wukongnalizou <37323494+wukongnalizou@users.noreply.github.com>
2019-07-03 15:42:14 +0800

    同步更新登陆认证 (#8)
    * Update README.md
    
    Fix several grammar and spelling issues.
    
    * 组织 (#1)
    
    * Fix a bug
    
    * 修改登陆认证 (#7)
    
    * 同步更新登陆认证

--- src/utils.js
+++ src/utils.js
@@ -65,8 +65,11 @@ function ajaxFactory(method) {
       req.addEventListener('error', error => reject(error))
     })
     req.open(method, url, true)
-
-    req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview, application/vnd.github.html+json')
+    if (apiPath === "https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token") {
+      req.setRequestHeader('Accept', 'application/json');
+    } else {
+      req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview, application/vnd.github.html+json');
+    }
     if (token) {
       req.setRequestHeader('Authorization', `token ${token}`)
     }
